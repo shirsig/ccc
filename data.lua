24a643934ab049ccc570f0d39e84cff7162d53c6
Manuel Simon Hirsig <simon.hirsig@gmail.com>
2017-01-11 22:35:32 +0100

    adapted gains

--- data.lua
+++ data.lua
@@ -365,10 +365,6 @@ ccwatch_EFFECTS = {
 	-- 	DURATION = {3},
 	-- },
 
-	["Adrenaline Rush"] = {
-		ICON = 'Spell_Shadow_ShadowWordDominate',
-		DURATION = {15},
-	},
 	["Sprint"] = {
 		ICON = 'Ability_Rogue_Sprint',
 		DURATION = {15},
@@ -377,29 +373,13 @@ ccwatch_EFFECTS = {
 		ICON = 'Spell_Shadow_ShadowWard',
 		DURATION = {15},
 	},
-	["Blade Flurry"] = {
-		ICON = 'Ability_GhoulFrenzy',
-		DURATION = {15},
-	},
 	["Power Word: Shield"] = {
 		ICON = 'Spell_Holy_PowerWordShield',
 		DURATION = {30},
 	},
-	["Power Infusion"] = {
-		ICON = 'Spell_Holy_PowerInfusion',
-		DURATION = {15},
-	},
-	["Arcane Power"] = {
-		ICON = 'Spell_Nature_Lightning',
-		DURATION = {15},
-	},
 	["Ice Block"] = {
 		ICON = 'Spell_Frost_Frost',
 		DURATION = {10},
-	},
-	["Nature's Grasp"] = {
-		ICON = 'Spell_Nature_NaturesWrath',
-		DURATION = {45},
 	},
 	["Dash"] = {
 		ICON = 'Ability_Druid_Dash',
@@ -409,17 +389,9 @@ ccwatch_EFFECTS = {
 		ICON = 'Spell_Nature_StoneClawTotem',
 		DURATION = {15},
 	},
-	["Tiger's Fury"] = {
-		ICON = 'Ability_Mount_JungleTiger',
-		DURATION = {6},
-	},
 	["Deterrence"] = {
 		ICON = 'Ability_Whirlwind',
 		DURATION = {10},
-	},
-	["Rapid Fire"] = {
-		ICON = 'Ability_Hunter_RunningShot',
-		DURATION = {15},
 	},
 	["Divine Shield"] = {
 		ICON = 'Spell_Holy_DivineIntervention',
@@ -445,24 +417,8 @@ ccwatch_EFFECTS = {
 		ICON = 'Ability_Racial_BloodRage',
 		DURATION = {10},
 	},
-	["Sweeping Strikes"] = {
-		ICON = 'Ability_Rogue_SliceDice',
-		DURATION = {20},
-	},
-	["Last Stand"] = {
-		ICON = 'Spell_Holy_AshesToAshes',
-		DURATION = {20},
-	},
 	["Retaliation"] = {
 		ICON = 'Ability_Warrior_Challange',
-		DURATION = {15},
-	},
-	["Shield Wall"] = {
-		ICON = 'Ability_Warrior_ShieldWall',
-		DURATION = {10},
-	},
-	["Recklessness"] = {
-		ICON = 'Ability_CriticalStrike',
 		DURATION = {15},
 	},
 	["Death Wish"] = {
@@ -477,4 +433,8 @@ ccwatch_EFFECTS = {
 		ICON = 'Spell_Nature_Sleep',
 		DURATION = {20},
 	},
+	["Fearless"] = {
+		ICON = 'Ability_Warrior_BattleShout',
+		DURATION = {30},
+	}
 }
